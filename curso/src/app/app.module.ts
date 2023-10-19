25959a5310be7298421a53da3328916a7556850c
Javier <jmartin@grupoloyal.com>
2023-10-19 12:20:49 +0200

    Laboratorio

--- curso/src/app/app.module.ts
+++ curso/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { DaskboardComponent } from './daskboard/daskboard.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { CommonComponentModule } from './common-component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { FormularioComponent } from './formulario/formulario.component';
     FormularioComponent,
   ],
   imports: [
-    BrowserModule, FormsModule,
+    BrowserModule, FormsModule, HttpClientModule,
     AppRoutingModule, MainModule, SecurityModule, MyCoreModule, CommonServicesModule,
     GraficoSvgComponent, CommonComponentModule,
   ],
